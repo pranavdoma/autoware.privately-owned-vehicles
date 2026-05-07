53053b91fef982f983b0e35d11196b1ffc8076c5
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-07 20:13:55 +0900

    big ass refactoring for auxiliaries

--- VisionPilot/development_releases/auxiliaries/camera_subscriber/video_publisher/include/video_publisher/video_file_publisher.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
-#define VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
-
-namespace video_publisher {
-
-}; // namespace video_publisher
-
-#endif // VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
